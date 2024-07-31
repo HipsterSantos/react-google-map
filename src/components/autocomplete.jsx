39848a3fbe3b5181cfc67b3105289b0f93f547b3
HipsterSantos <santoscampos269@gmail.com>
2024-07-31 16:10:46 +0100

    [update]  adding boundaries search

--- src/components/autocomplete.jsx
+++ src/components/autocomplete.jsx
@@ -9,42 +9,51 @@ export default function Autocomplete({onPlaceSelected, ...rest}){
         googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY,
         libraries
     })
-    const [location,setLocation] = useState({lat: null, lon: null})
-    const [country,setCountry] = useState(null)
+    const [location,setLocation] = useState({lat: null, lng: null})
+    const [country,setCountry] = useState('AO')
 
     const inputRef = useRef(null)
     const autocomplete = useRef(null)
 
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(
-            async (position)=>{
-                
-                console.log('\n\nPosition-data-coords ', position)
+    useEffect(()=>{
 
-                const { latitude, longitude } = position.coords;
-                const response = await fetch(
-                    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${import.meta.env.VITE_GOOGLE_MAP_API_KEY}`
-                  );
-                
-                  const data = await response.json();
-                  if(data.results.length>0){
-                    const addressComponents = data.results[0].address_components;
-                    const countryComponent = addressComponents.find(component =>
-                      component.types.includes('country')
-                    );
-                    setCountry(countryComponent.short_name);
-                  }
-                
-                  console.log('\n\ndata-feched-current-location ', data)
-            },
-            (error)=>console.log)
-    }
+        if(navigator.geolocation){
+            navigator.geolocation.getCurrentPosition(
+                async (position)=>{
+                    
+                    console.log('\n\nPosition-data-coords ', position)
+    
+                    const { latitude, longitude } = position.coords;
+                    setLocation({lat: latitude,lng: longitude})
+                    const response = await fetch(
+                        `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${import.meta.env.VITE_GOOGLE_MAP_API_KEY}`
+                      );
+                    
+                      const data = await response.json();
+                      if(data.results.length>0){
+                        const addressComponents = data.results[0].address_components;
+                        const countryComponent = addressComponents.find(component =>
+                          component.types.includes('country')
+                        );
+                        setCountry(countryComponent.short_name);
+                      }
+                    
+                      console.log('\n\ndata-feched-current-location ', data)
+                },
+                (error)=>console.log(error))
+        }
+    },[])
+
 
     useEffect(()=>{
         if(isLoaded && inputRef.current){
             autocomplete.current = new window.google.maps.places.Autocomplete(inputRef.current,{
-                types: ['(cities)'],
-                componentRestrictions:{country}
+                types: ['geocode'],
+                componentRestrictions:{country},
+                strictBounds: false,
+                // bounds: new window.google.maps.LatLngBounds(
+                //     new window.google.maps.LatLng(location.lat -0.1, location.lng - 0.1),
+                //     new window.google.maps.LatLng(location.lat - 0.1,location.lng - 0.1) )
             })
             autocomplete.current.addListener('place_changed',()=>{
                 const place = autocomplete.current.getPlace();
