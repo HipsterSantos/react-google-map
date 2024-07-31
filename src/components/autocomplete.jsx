fc6982c0b48fff8af93966311cb8905f520c4294
HipsterSantos <santoscampos269@gmail.com>
2024-07-31 15:22:18 +0100

    [improvments] improving google map accuracy

--- src/components/autocomplete.jsx
+++ src/components/autocomplete.jsx
@@ -6,7 +6,7 @@ const libraries = ['places']
 
 export default function Autocomplete({onPlaceSelected, ...rest}){
     const {isLoaded,loadError } = useJsApiLoader({
-        googleMapsApiKey: import.meta.env.GOOGLE_MAP_API_KEY,
+        googleMapsApiKey: import.meta.env.VITE_GOOGLE_MAP_API_KEY,
         libraries
     })
     const [location,setLocation] = useState({lat: null, lon: null})
@@ -15,10 +15,36 @@ export default function Autocomplete({onPlaceSelected, ...rest}){
     const inputRef = useRef(null)
     const autocomplete = useRef(null)
 
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(
+            async (position)=>{
+                
+                console.log('\n\nPosition-data-coords ', position)
+
+                const { latitude, longitude } = position.coords;
+                const response = await fetch(
+                    `https://maps.googleapis.com/maps/api/geocode/json?latlng=${latitude},${longitude}&key=${import.meta.env.VITE_GOOGLE_MAP_API_KEY}`
+                  );
+                
+                  const data = await response.json();
+                  if(data.results.length>0){
+                    const addressComponents = data.results[0].address_components;
+                    const countryComponent = addressComponents.find(component =>
+                      component.types.includes('country')
+                    );
+                    setCountry(countryComponent.short_name);
+                  }
+                
+                  console.log('\n\ndata-feched-current-location ', data)
+            },
+            (error)=>console.log)
+    }
+
     useEffect(()=>{
         if(isLoaded && inputRef.current){
             autocomplete.current = new window.google.maps.places.Autocomplete(inputRef.current,{
-                types: ['locality']
+                types: ['(cities)'],
+                componentRestrictions:{country}
             })
             autocomplete.current.addListener('place_changed',()=>{
                 const place = autocomplete.current.getPlace();
